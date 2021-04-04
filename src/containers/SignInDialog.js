d9e7d346be04affe365bfcbb287e245175ba3935
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-04-04 23:49:22 +0900

    feat: redux 스토어 생성 및 persist 라이브러리 추가

--- src/containers/SignInDialog.js
+++ src/containers/SignInDialog.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Dialog from 'components/Dialog/Dialog';
 import { MotionButton } from 'components/Button/Button.styled';
 import AvatarSettingBox from './AvatarSettingBox/AvatarSettingBox';
@@ -8,10 +10,16 @@ import AvatarSettingBox from './AvatarSettingBox/AvatarSettingBox';
 export default function SignInDialog({
   visible = false, // 다이얼로그 가시성 여부
 }) {
+
+  const currentUser = useSelector(state => state.currentUser);
+  const dispatch = useDispatch();
+
   return (
     <Dialog visible={visible} infoText="Start with your avatar!">
       <AvatarSettingBox initValue="" id="dialogInput" />
-      <MotionButton>DONE</MotionButton>
+      <MotionButton
+        // onClick={}
+      >DONE</MotionButton>
     </Dialog>
   );
 }
